63d37a1d1a2310eb08346277715fe5e59edf010a
Steven Boutcher <sjboutcher@gmail.com>
2019-03-11 21:30:39 -0500

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# fcc-fel5
-5th project for freeCodeCamp's Front End Libraries Certification -- React Pomodoro Clock
---
+++ precache-manifest.5596f017f29584f8e876b5074d0240f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fcc-fel5/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d7875cdef52eb9c0d400",
+    "url": "/fcc-fel5/static/js/runtime~main.2a4b6d0d.js"
+  },
+  {
+    "revision": "932a4b65f3ae6b5f429b",
+    "url": "/fcc-fel5/static/js/main.970ea055.chunk.js"
+  },
+  {
+    "revision": "17d6eb59b6c003d2b289",
+    "url": "/fcc-fel5/static/js/2.7baffc28.chunk.js"
+  },
+  {
+    "revision": "932a4b65f3ae6b5f429b",
+    "url": "/fcc-fel5/static/css/main.2796b27c.chunk.css"
+  },
+  {
+    "revision": "5c78c383b8fbc7a8c62d6027230853f5",
+    "url": "/fcc-fel5/index.html"
+  }
+];
